9cd4c5a91f90c791a32591721bf9a63d9470f005
JollyRen <jollyren@gmail.com>
2022-04-25 16:29:21 +0200

    editor config added, prettier config, tailwinds, postcss config, main.css, views- blogview, portfolioview

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import PortfolioView from '../views/PortfolioView.vue'
+import BlogView from '../views/BlogView.vue'
 
 const routes = [
   {
@@ -9,11 +12,18 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'About',
+    component: AboutView
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: PortfolioView
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: BlogView
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="jeremy-pic" src="../assets/profilePic.png" />
   </div>
+  <section>
+    <article>
+      <h1>Welcome!</h1>
+      <p>Hello, I'm Jeremy Rogers, a full-stack software developer and graphic designer.</p>
+      <p>
+        —
+        <br />
+        Do you need someone able to combine your technology stack, UI/UX, branding, and
+        accessibility in one?
+      </p>
+      <p>
+        <strong>Since 2013 I've worked in:</strong>
+        <br />
+        Graphic Design
+        <br />
+        Web Design
+        <br />
+        App Design
+        <br />
+        SEO Content Writing
+        <br />
+        Software Development
+      </p>
+      <p>
+        <strong>I'm proficient in JavaScript applications using technologies such as:</strong>
+        <br />
+        React || Vue || Express.js || Node.js || PostgreSQL || Google Cloud Platform || Firebase ||
+        NoSQL || Firestore || Electron.js || Electron.js || Redux || and more...
+      </p>
+      <p>
+        Want to know exactly
+        <router-link to="/blog">what all that means</router-link> and what I get up to?
+      </p>
+      <router-link to="/portfolio">Check out my portfolio!</router-link>
+      <p></p>
+    </article>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
